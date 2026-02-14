129b15224cf56f4d5f096c83b982b5328e2bb302
Andrei Mihai <andreimihai2305is@gmail.com>
2026-02-14 10:55:36 +0200

    added scrolling into view

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+let button = document.getElementById("hero-button1");
+let servicesSection = document.getElementById("servicii");
+
+button?.addEventListener("click", () => {
+    servicesSection?.scrollIntoView({
+        behavior: "smooth"
+    });
+});
