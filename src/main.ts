3040e634e5ec46739f9d2bcf160642d2e87c5911
Andrei Mihai <andreimihai2305is@gmail.com>
2026-02-20 12:46:40 +0200

    small modifications to section and more animations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import "./styles.css";
 
 const heroButton = document.getElementById("hero-button1");
-
+const serviciiLink = document.getElementById("servicii-link");
 const servicesSection = document.getElementById("servicii");
+
+const serviciiPageTitle = document.getElementById("servicii-title");
+
+if (serviciiPageTitle != null)
+{
+    serviciiLink?.removeAttribute("href");
+    serviciiLink?.addEventListener("click", () => {
+        serviciiPageTitle?.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+    
+}
 
 heroButton?.addEventListener("click", () => {
     servicesSection?.scrollIntoView({
@@ -10,11 +23,6 @@ heroButton?.addEventListener("click", () => {
     });
 });
 
-// serviciiLink?.addEventListener("click", () => {
-//     servicesSection?.scrollIntoView({
-//         behavior: "smooth"
-//     });
-// });
 
 const footerYear = document.getElementById('year'); 
 if (footerYear != null)
