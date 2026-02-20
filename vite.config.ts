ff48c8e7b5ba9ef5847b042c7017179661716ad7
Andrei Mihai <andreimihai2305is@gmail.com>
2026-02-20 10:08:43 +0200

    fixed gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
   base: '/Brigada-Electrica/',
-})
+  build: {
+    rollupOptions : {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        servicii: resolve(__dirname, "servicii.html")
+      },
+    },
+  },
+});
