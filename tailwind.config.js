dd95f72771e70785e3b0f97823045d574b2e6887
Andrei Mihai <andreimihai2305is@gmail.com>
2026-02-21 10:51:23 +0200

    started work on todos

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./servicii.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+ theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
