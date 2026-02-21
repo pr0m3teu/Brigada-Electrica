aff1a0c1d52c4ddcc7db1b8c5b2b95f0c449735f
Andrei Mihai <andreimihai2305is@gmail.com>
2026-02-21 20:02:25 +0200

    worked some more on responsiveness and small tweaks

--- src/animations.ts
+++ src/animations.ts
@@ -5,16 +5,20 @@ export function fadeIn(elements: NodeList) : void
         threshold: 0.15
     };
 
-    const observer = new IntersectionObserver((entries) =>
+    const observer = new IntersectionObserver((entries, observer) =>
     {
         entries.forEach(entry => {
             if(entry.isIntersecting)
             {
                 entry.target.classList.remove("opacity-0", "translate-y-8");
                 entry.target.classList.add("opacity-100", "translate-y-0");
+                if(!(entry.target instanceof HTMLButtonElement))
+                {
+                    observer.unobserve(entry.target);
+                }
             }
 
-            if(!entry.isIntersecting)
+            if(!entry.isIntersecting )
             {
                 entry.target.classList.remove("opacity-100", "translate-y-0");
                 entry.target.classList.add("opacity-0", "translate-y-8");
